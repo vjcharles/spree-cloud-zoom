492a7c45a69187d25305ba5cfcf6bfd68c53aaf0
Vincent Charles <vjcharles@gmail.com>
2011-02-23 04:45:30 -0800

    added cloud zoom files

--- lib/spree_cloud_zoom_hooks.rb
+++ lib/spree_cloud_zoom_hooks.rb
@@ -1,3 +1,5 @@
 class SpreeCloudZoomHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
+  replace :product_images, 'products/product_images'
+  insert_after :inside_head, 'shared/cloud_zoom_include'
 end
